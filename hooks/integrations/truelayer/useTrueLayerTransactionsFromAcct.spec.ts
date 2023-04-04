38599e73f3fe0f9bee718b4ff0c8adaa33475bf3
Dillon Montag <dillon@thoughtmachine.net>
2023-04-04 08:43:29 +0100

    Change name of truelayer hook

--- hooks/integrations/truelayer/useTrueLayerTransactionsFromAcct.spec.ts
+++ hooks/integrations/truelayer/useTrueLayerTransactionsFromAcct.spec.ts
@@ -1,6 +1,6 @@
 import {renderHook, waitFor} from "@testing-library/react-native";
 
-import useTrueLayerTransactionsFromAcct from "./useTrueLayerTransactions";
+import useTrueLayerTransactionsFromAcct from "./useTrueLayerTransactionsFromAcct";
 
 import {trueLayerDataApi} from "../../../axiosConfig";
 import {
