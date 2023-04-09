f040e645a00f2d0870f54083c02e29b1078eb0c4
Dillon Montag <dillon@thoughtmachine.net>
2023-04-09 22:23:31 +0100

    Add integration test for transactions

---
+++ tests/integration/transactions.spec.ts
@@ -0,0 +1,117 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {renderHook, waitFor} from "@testing-library/react-native";
+
+import {trueLayerDataApi} from "../../axiosConfig";
+import useTransactions from "../../hooks/useTransactions";
+import {TransactionCategory} from "../../types/transaction";
+import {
+  TRUELAYER_EATING_OUT_CARD_TRANSACTION_MINIMUM_FIELDS,
+  TRUELAYER_PAY_BILL_CARD_TRANSACTION_ALL_FIELDS
+} from "../mocks/trueLayer/dataAPI/data/cardData";
+import {tanstackQueryTestWrapper} from "../mocks/utils";
+
+jest.mock("../../axiosConfig");
+
+describe("transaction flow", () => {
+  test("returns empty values when no truelayer transactions exist", async () => {
+    // setup mocks
+    const mockTrueLayerDataApi = trueLayerDataApi as jest.MockedObject<
+      typeof trueLayerDataApi
+    >;
+    mockTrueLayerDataApi.get.mockImplementationOnce(async () => []);
+
+    const {result} = renderHook(() => useTransactions("dummy"), {
+      wrapper: tanstackQueryTestWrapper
+    });
+    await waitFor(() => expect(result.current.isLoading).toBe(false));
+    expect(result.current.transactions).toEqual([]);
+    expect(await AsyncStorage.getAllKeys()).toEqual([]);
+  });
+
+  test("stores default category values", async () => {
+    // setup mocks
+    const mockTrueLayerDataApi = trueLayerDataApi as jest.MockedObject<
+      typeof trueLayerDataApi
+    >;
+    mockTrueLayerDataApi.get.mockImplementationOnce(async () => [
+      TRUELAYER_PAY_BILL_CARD_TRANSACTION_ALL_FIELDS,
+      TRUELAYER_EATING_OUT_CARD_TRANSACTION_MINIMUM_FIELDS
+    ]);
+
+    const {result} = renderHook(() => useTransactions("dummy"), {
+      wrapper: tanstackQueryTestWrapper
+    });
+    await waitFor(() => expect(result.current.isLoading).toBe(false));
+    expect(result.current.transactions).toEqual([
+      {
+        name: "PAY OFF CREDIT CARD BILL",
+        description: "Bills and Utilities",
+        amount: 192.52,
+        category: TransactionCategory.BILLS
+      },
+      {
+        name: "CHIPOTLE AIRPORT BLVD",
+        description: "Food & Dining",
+        amount: 36.71,
+        category: TransactionCategory.EATING_OUT
+      }
+    ]);
+    expect(
+      await AsyncStorage.multiGet([
+        "truelayer-a15d8156569ba848d84c07c34d291bca",
+        "truelayer-1234094-shocking-chipotle"
+      ])
+    ).toEqual([
+      ["truelayer-a15d8156569ba848d84c07c34d291bca", TransactionCategory.BILLS],
+      ["truelayer-1234094-shocking-chipotle", TransactionCategory.EATING_OUT]
+    ]);
+  });
+
+  test("merges transaction categories from storage", async () => {
+    // setup mocks
+    const mockTrueLayerDataApi = trueLayerDataApi as jest.MockedObject<
+      typeof trueLayerDataApi
+    >;
+    mockTrueLayerDataApi.get.mockImplementationOnce(async () => [
+      TRUELAYER_PAY_BILL_CARD_TRANSACTION_ALL_FIELDS,
+      TRUELAYER_EATING_OUT_CARD_TRANSACTION_MINIMUM_FIELDS
+    ]);
+
+    // setup data in Async Storage
+    await AsyncStorage.setItem(
+      "truelayer-a15d8156569ba848d84c07c34d291bca",
+      TransactionCategory.SAVINGS
+    );
+
+    const {result} = renderHook(() => useTransactions("dummy"), {
+      wrapper: tanstackQueryTestWrapper
+    });
+    await waitFor(() => expect(result.current.isLoading).toBe(false));
+    expect(result.current.transactions).toEqual([
+      {
+        name: "PAY OFF CREDIT CARD BILL",
+        description: "Bills and Utilities",
+        amount: 192.52,
+        category: TransactionCategory.SAVINGS
+      },
+      {
+        name: "CHIPOTLE AIRPORT BLVD",
+        description: "Food & Dining",
+        amount: 36.71,
+        category: TransactionCategory.EATING_OUT
+      }
+    ]);
+    expect(
+      await AsyncStorage.multiGet([
+        "truelayer-a15d8156569ba848d84c07c34d291bca",
+        "truelayer-1234094-shocking-chipotle"
+      ])
+    ).toEqual([
+      [
+        "truelayer-a15d8156569ba848d84c07c34d291bca",
+        TransactionCategory.SAVINGS
+      ],
+      ["truelayer-1234094-shocking-chipotle", TransactionCategory.EATING_OUT]
+    ]);
+  });
+});
