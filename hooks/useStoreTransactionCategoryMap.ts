c068e9d89cb3740eaef350951c830ce6c1d6f693
Dillon Montag <dillon@thoughtmachine.net>
2023-04-23 17:57:56 +0100

    Remove optimistic update

--- hooks/useStoreTransactionCategoryMap.ts
+++ hooks/useStoreTransactionCategoryMap.ts
@@ -18,44 +18,6 @@ const useStoreTransactionCategoryMap = () => {
 
   return useMutation({
     mutationFn: storeTransactionCategoryMapping,
-    onMutate: async newMapping => {
-      if (!Object.keys(newMapping).length) return;
-      const queryKey = [
-        "transactionCategoryMapping",
-        ...Object.keys(newMapping)
-      ];
-      // Cancel any outgoing re-fetches for any transaction category mappings
-      // so they don't overwrite the optimistic update
-      await queryClient.cancelQueries({
-        queryKey: ["transactionCategoryMapping"]
-      });
-
-      // Snapshot the previous value
-      const previousMapping =
-        queryClient.getQueryData<TransactionIDToCategoryMapping>(queryKey);
-
-      // Optimistically update to the new value
-      queryClient.setQueryData<TransactionIDToCategoryMapping>(
-        queryKey,
-        () => newMapping
-      );
-
-      // Return a context object with the snapshotted value
-      return {previousMapping};
-    },
-    // If the mutation fails, use the context returned from onMutate to roll back
-    onError: (_, variables, context) => {
-      const previousCacheValue = context?.previousMapping;
-      previousCacheValue
-        ? queryClient.setQueryData<TransactionIDToCategoryMapping>(
-            ["transactionCategoryMapping", ...Object.keys(variables)],
-            previousCacheValue
-          )
-        : queryClient.removeQueries([
-            "transactionCategoryMapping",
-            ...Object.keys(variables)
-          ]);
-    },
     // Always refetch all transaction category mappings after success or error
     onSettled: () => {
       queryClient.invalidateQueries({
