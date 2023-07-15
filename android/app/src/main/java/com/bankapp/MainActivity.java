03636586035afc814638bbf15cd4eb721e93eebc
Dillon Montag <dillon@thoughtmachine.net>
2023-07-15 11:38:25 +0100

    Install react navigation

--- android/app/src/main/java/com/bankapp/MainActivity.java
+++ android/app/src/main/java/com/bankapp/MainActivity.java
@@ -1,11 +1,21 @@
 package com.bankapp;
 
+// added the next import following the react navigation getting started guide
+// https://reactnavigation.org/docs/getting-started/
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
+
+  // added following the react navigation getting started guide
+  // https://reactnavigation.org/docs/getting-started/
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
