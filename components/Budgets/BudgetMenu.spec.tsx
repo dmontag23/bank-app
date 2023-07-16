9f13fa69b67dd202a938a7ea7d58b795d819900d
Dillon Montag <dillon@thoughtmachine.net>
2023-07-16 20:43:10 +0100

    Add BudgetMenu component

---
+++ components/Budgets/BudgetMenu.spec.tsx
@@ -0,0 +1,242 @@
+import React from "react";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor
+} from "@testing-library/react-native";
+
+import BudgetMenu from "./BudgetMenu";
+
+import useDeleteBudget from "../../hooks/budgets/useDeleteBudget";
+import useGetAllBudgets from "../../hooks/budgets/useGetAllBudgets";
+import {
+  BUDGET_WITH_NO_ITEMS,
+  BUDGET_WITH_ONE_ITEM
+} from "../../tests/mocks/data/budgets";
+import {ComponentTestWrapper} from "../../tests/mocks/utils";
+
+jest.mock("../../hooks/budgets/useDeleteBudget");
+jest.mock("../../hooks/budgets/useGetAllBudgets");
+
+describe("BudgetMenu component", () => {
+  test("renders a fragment when budget data is undefined", () => {
+    // TODO: any should probably not be used as a type here, but since a
+    // query from tanstack query returns a whole bunch of non-optional things,
+    // it's quicker than returning all those things for now
+    const renderMenuIcon = jest.fn();
+    (useGetAllBudgets as jest.MockedFunction<any>).mockImplementation(() => ({
+      data: undefined
+    }));
+    (useDeleteBudget as jest.MockedFunction<any>).mockImplementation(() => ({
+      mutate: () => {}
+    }));
+
+    render(
+      <BudgetMenu
+        renderMenuIcon={renderMenuIcon}
+        setSelectedBudget={() => {}}
+      />,
+      {
+        wrapper: ComponentTestWrapper
+      }
+    );
+
+    expect(renderMenuIcon).toBeCalledTimes(1);
+    expect(renderMenuIcon).toBeCalledWith(expect.any(Function));
+  });
+
+  test("renders a fragment when budget data array does not contain budgets", () => {
+    // TODO: any should probably not be used as a type here, but since a
+    // query from tanstack query returns a whole bunch of non-optional things,
+    // it's quicker than returning all those things for now
+    const renderMenuIcon = jest.fn();
+    (useGetAllBudgets as jest.MockedFunction<any>).mockImplementation(() => ({
+      data: []
+    }));
+    (useDeleteBudget as jest.MockedFunction<any>).mockImplementation(() => ({
+      mutate: () => {}
+    }));
+
+    render(
+      <BudgetMenu
+        renderMenuIcon={renderMenuIcon}
+        setSelectedBudget={() => {}}
+      />,
+      {
+        wrapper: ComponentTestWrapper
+      }
+    );
+
+    expect(renderMenuIcon).toBeCalledTimes(1);
+    expect(renderMenuIcon).toBeCalledWith(expect.any(Function));
+  });
+
+  test("renders budgets", async () => {
+    // TODO: any should probably not be used as a type here, but since a
+    // query from tanstack query returns a whole bunch of non-optional things,
+    // it's quicker than returning all those things for now
+    const renderMenuIcon = jest.fn();
+    (useGetAllBudgets as jest.MockedFunction<any>).mockImplementation(() => ({
+      data: [BUDGET_WITH_NO_ITEMS, BUDGET_WITH_ONE_ITEM]
+    }));
+    (useDeleteBudget as jest.MockedFunction<any>).mockImplementation(() => ({
+      mutate: () => {}
+    }));
+
+    render(
+      <BudgetMenu
+        renderMenuIcon={renderMenuIcon}
+        setSelectedBudget={() => {}}
+      />,
+      {
+        wrapper: ComponentTestWrapper
+      }
+    );
+
+    expect(renderMenuIcon).toBeCalledTimes(1);
+    expect(renderMenuIcon).toBeCalledWith(expect.any(Function));
+
+    // open the menu
+    const openMenu = renderMenuIcon.mock.calls[0][0];
+    await act(() => openMenu());
+
+    // TODO: Investigate why toBeVisible() fails here
+    // I think it's because the opacity of a parent is 0
+    expect(screen.getByText(BUDGET_WITH_NO_ITEMS.name)).toBeOnTheScreen();
+    expect(screen.getByText(BUDGET_WITH_ONE_ITEM.name)).toBeOnTheScreen();
+
+    // check that there are two icon buttons
+    const iconButtons = screen.getAllByRole("button");
+    expect(iconButtons.length).toBe(2);
+  });
+
+  test("correctly selects a budget", async () => {
+    // TODO: any should probably not be used as a type here, but since a
+    // query from tanstack query returns a whole bunch of non-optional things,
+    // it's quicker than returning all those things for now
+    const renderMenuIcon = jest.fn();
+    const setSelectedBudget = jest.fn();
+    (useGetAllBudgets as jest.MockedFunction<any>).mockImplementation(() => ({
+      data: [BUDGET_WITH_NO_ITEMS, BUDGET_WITH_ONE_ITEM]
+    }));
+    (useDeleteBudget as jest.MockedFunction<any>).mockImplementation(() => ({
+      mutate: () => {}
+    }));
+
+    render(
+      <BudgetMenu
+        renderMenuIcon={renderMenuIcon}
+        setSelectedBudget={setSelectedBudget}
+      />,
+      {
+        wrapper: ComponentTestWrapper
+      }
+    );
+
+    expect(renderMenuIcon).toBeCalledTimes(1);
+    expect(renderMenuIcon).toBeCalledWith(expect.any(Function));
+
+    // open the menu
+    const openMenu = renderMenuIcon.mock.calls[0][0];
+    await act(() => openMenu());
+
+    const secondBudget = screen.getByText(BUDGET_WITH_ONE_ITEM.name);
+    // TODO: Investigate why toBeVisible() fails here
+    // I think it's because the opacity of a parent is 0
+    expect(secondBudget).toBeOnTheScreen();
+
+    fireEvent.press(secondBudget);
+
+    await waitFor(() => expect(secondBudget).not.toBeOnTheScreen());
+    expect(setSelectedBudget).toBeCalledTimes(1);
+    expect(setSelectedBudget).toBeCalledWith(BUDGET_WITH_ONE_ITEM);
+  });
+
+  test("correctly deletes a budget and keeps current budget selection", async () => {
+    // TODO: any should probably not be used as a type here, but since a
+    // query from tanstack query returns a whole bunch of non-optional things,
+    // it's quicker than returning all those things for now
+    const renderMenuIcon = jest.fn();
+    const setSelectedBudget = jest.fn();
+    (useGetAllBudgets as jest.MockedFunction<any>).mockImplementation(() => ({
+      data: [BUDGET_WITH_NO_ITEMS, BUDGET_WITH_ONE_ITEM]
+    }));
+    (useDeleteBudget as jest.MockedFunction<any>).mockImplementation(() => ({
+      mutate: () => {}
+    }));
+
+    render(
+      <BudgetMenu
+        renderMenuIcon={renderMenuIcon}
+        setSelectedBudget={setSelectedBudget}
+      />,
+      {
+        wrapper: ComponentTestWrapper
+      }
+    );
+
+    expect(renderMenuIcon).toBeCalledTimes(1);
+    expect(renderMenuIcon).toBeCalledWith(expect.any(Function));
+
+    // open the menu
+    const openMenu = renderMenuIcon.mock.calls[0][0];
+    await act(() => openMenu());
+
+    // check that there are two icon buttons
+    const iconButtons = screen.getAllByRole("button");
+    expect(iconButtons.length).toBe(2);
+    const deleteSecondBudgetButton = iconButtons[1];
+
+    fireEvent.press(deleteSecondBudgetButton);
+
+    expect(setSelectedBudget).toBeCalledTimes(1);
+    const selectedBudget =
+      setSelectedBudget.mock.calls[0][0](BUDGET_WITH_NO_ITEMS);
+    expect(selectedBudget).toBe(BUDGET_WITH_NO_ITEMS);
+  });
+
+  test("correctly deletes a budget and de-selects same budget", async () => {
+    // TODO: any should probably not be used as a type here, but since a
+    // query from tanstack query returns a whole bunch of non-optional things,
+    // it's quicker than returning all those things for now
+    const renderMenuIcon = jest.fn();
+    const setSelectedBudget = jest.fn();
+    (useGetAllBudgets as jest.MockedFunction<any>).mockImplementation(() => ({
+      data: [BUDGET_WITH_NO_ITEMS, BUDGET_WITH_ONE_ITEM]
+    }));
+    (useDeleteBudget as jest.MockedFunction<any>).mockImplementation(() => ({
+      mutate: () => {}
+    }));
+
+    render(
+      <BudgetMenu
+        renderMenuIcon={renderMenuIcon}
+        setSelectedBudget={setSelectedBudget}
+      />,
+      {
+        wrapper: ComponentTestWrapper
+      }
+    );
+
+    expect(renderMenuIcon).toBeCalledTimes(1);
+    expect(renderMenuIcon).toBeCalledWith(expect.any(Function));
+
+    // open the menu
+    const openMenu = renderMenuIcon.mock.calls[0][0];
+    await act(() => openMenu());
+
+    // check that there are two icon buttons
+    const iconButtons = screen.getAllByRole("button");
+    expect(iconButtons.length).toBe(2);
+    const deleteSecondBudgetButton = iconButtons[1];
+
+    fireEvent.press(deleteSecondBudgetButton);
+
+    expect(setSelectedBudget).toBeCalledTimes(1);
+    const selectedBudget =
+      setSelectedBudget.mock.calls[0][0](BUDGET_WITH_ONE_ITEM);
+    expect(selectedBudget).toBeNull();
+  });
+});
---
+++ components/Budgets/BudgetMenu.tsx
@@ -0,0 +1,65 @@
+import React, {useState} from "react";
+import {GestureResponderEvent, StyleSheet, View} from "react-native";
+import {IconButton, Menu, useTheme} from "react-native-paper";
+
+import useDeleteBudget from "../../hooks/budgets/useDeleteBudget";
+import useGetAllBudgets from "../../hooks/budgets/useGetAllBudgets";
+import {Budget} from "../../types/budget";
+
+type BudgetMenuProps = {
+  renderMenuIcon: (openMenu: (e: GestureResponderEvent) => void) => JSX.Element;
+  setSelectedBudget: React.Dispatch<React.SetStateAction<Budget | null>>;
+};
+
+const BudgetMenu = ({renderMenuIcon, setSelectedBudget}: BudgetMenuProps) => {
+  const theme = useTheme();
+
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const openMenu = () => setIsMenuVisible(true);
+  const closeMenu = () => setIsMenuVisible(false);
+
+  const {data: budgets} = useGetAllBudgets();
+  const {mutate: deleteBudget} = useDeleteBudget();
+
+  const menuIcon = renderMenuIcon(openMenu);
+
+  // TODO: Should probably return a loading spinner as a menu
+  // item if the budgets are still loading
+  return budgets?.length ? (
+    <Menu
+      visible={isMenuVisible}
+      onDismiss={closeMenu}
+      anchor={menuIcon}
+      anchorPosition="bottom">
+      {budgets.map(budget => (
+        <View key={budget.id} style={styles.menuItemContainer}>
+          <Menu.Item
+            onPress={() => {
+              setSelectedBudget(budget);
+              closeMenu();
+            }}
+            title={budget.name}
+          />
+          <IconButton
+            icon="trash-can"
+            iconColor={theme.colors.error}
+            onPress={() => {
+              setSelectedBudget(selectedBudget =>
+                selectedBudget?.id === budget.id ? null : selectedBudget
+              );
+              deleteBudget(budget.id);
+            }}
+          />
+        </View>
+      ))}
+    </Menu>
+  ) : (
+    <></>
+  );
+};
+
+const styles = StyleSheet.create({
+  menuItemContainer: {flexDirection: "row", justifyContent: "space-between"}
+});
+
+export default BudgetMenu;
