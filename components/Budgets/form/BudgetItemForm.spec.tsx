9ae429b762dc90c2aef5646513ae2f398fc8586e
Dillon Montag <dillon@thoughtmachine.net>
2023-08-05 16:18:41 +0100

    Fix BudgetItemForm tests

--- components/Budgets/form/BudgetItemForm.spec.tsx
+++ components/Budgets/form/BudgetItemForm.spec.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import {View} from "react-native";
-import {beforeEach, describe, expect, jest, test} from "@jest/globals";
-import {fireEvent, render, screen} from "@testing-library/react-native";
+import {useForm} from "react-hook-form";
+import {describe, expect, jest, test} from "@jest/globals";
+import {
+  fireEvent,
+  render,
+  renderHook,
+  screen
+} from "@testing-library/react-native";
 
 import BudgetItemForm from "./BudgetItemForm";
 import BudgetItemFormFields from "./BudgetItemFormFields";
@@ -18,23 +23,11 @@ jest.mock("./BudgetItemFormFields");
 jest.mock("../../ui/ExpandableAccordion");
 
 describe("BudgetItemForm component", () => {
-  const MOCK_ACCORDION = jest.fn<typeof ExpandableAccordion>();
-  const MOCK_BUDGET_ITEM_FORM_FIELDS = jest.fn();
-  const EMPTY_BUDGET: BudgetInput = {
-    id: "1",
+  const BUDGET_WITH_ONE_EMPTY_ITEM: BudgetInput = {
+    id: "budget-1",
     name: "",
-    window: {
-      start: new Date("2023-01-01"),
-      end: new Date("2023-01-31")
-    },
-    items: [
-      {
-        id: "",
-        name: "",
-        categories: [],
-        cap: ""
-      }
-    ]
+    window: {start: new Date("01-01-2023"), end: new Date("01-02-2023")},
+    items: [{id: "dummy", name: "", cap: "", categories: []}]
   };
 
   const ITEM_1: BudgetItemInput = {
@@ -44,61 +37,41 @@ describe("BudgetItemForm component", () => {
     cap: ""
   };
 
-  beforeEach(() => {
-    (
-      ExpandableAccordion as jest.MockedFunction<typeof ExpandableAccordion>
-    ).mockImplementation(props => {
-      MOCK_ACCORDION(props);
-      return <View testID={`accordion-${props.title}`} />;
-    });
-    (
-      BudgetItemFormFields as jest.MockedFunction<typeof BudgetItemFormFields>
-    ).mockImplementation(props => {
-      MOCK_BUDGET_ITEM_FORM_FIELDS(props);
-      return <View testID={`budgetItemFormFields-${props.budgetItem.id}`} />;
-    });
-  });
-
-  test("renders all components", async () => {
-    render(<BudgetItemForm budget={EMPTY_BUDGET} setBudget={() => {}} />, {
-      wrapper: ComponentTestWrapper
-    });
-
-    expect(screen.getByTestId("accordion-Budget Item")).toBeVisible();
-    expect(screen.getByText("Add item")).toBeVisible();
-  });
-
   test("has the correct accordion text when no item name is provided", async () => {
-    render(<BudgetItemForm budget={EMPTY_BUDGET} setBudget={() => {}} />, {
+    const {result} = renderHook(() =>
+      useForm<BudgetInput>({defaultValues: BUDGET_WITH_ONE_EMPTY_ITEM})
+    );
+
+    render(<BudgetItemForm control={result.current.control} />, {
       wrapper: ComponentTestWrapper
     });
 
-    expect(screen.getByTestId("accordion-Budget Item")).toBeVisible();
-    expect(MOCK_ACCORDION).toBeCalledTimes(1);
-    expect(MOCK_ACCORDION).toBeCalledWith(
+    expect(ExpandableAccordion).toBeCalledTimes(1);
+    expect(ExpandableAccordion).toBeCalledWith(
       expect.objectContaining({
         title: "Budget Item"
-      })
+      }),
+      {}
     );
   });
 
   test("has the correct accordion text when an item name is provided", async () => {
-    render(
-      <BudgetItemForm
-        budget={{...EMPTY_BUDGET, items: [ITEM_1]}}
-        setBudget={() => {}}
-      />,
-      {
-        wrapper: ComponentTestWrapper
-      }
+    const {result} = renderHook(() =>
+      useForm<BudgetInput>({
+        defaultValues: {...BUDGET_WITH_ONE_EMPTY_ITEM, items: [ITEM_1]}
+      })
     );
 
-    expect(screen.getByTestId("accordion-Item 1")).toBeVisible();
-    expect(MOCK_ACCORDION).toBeCalledTimes(1);
-    expect(MOCK_ACCORDION).toBeCalledWith(
+    render(<BudgetItemForm control={result.current.control} />, {
+      wrapper: ComponentTestWrapper
+    });
+
+    expect(ExpandableAccordion).toBeCalledTimes(1);
+    expect(ExpandableAccordion).toBeCalledWith(
       expect.objectContaining({
         title: "Item 1"
-      })
+      }),
+      {}
     );
   });
 
@@ -117,85 +90,61 @@ describe("BudgetItemForm component", () => {
     };
 
     const budget: BudgetInput = {
-      ...EMPTY_BUDGET,
+      ...BUDGET_WITH_ONE_EMPTY_ITEM,
       items: [ITEM_1, item2, item3]
     };
-    const setBudget =
-      jest.fn<React.Dispatch<React.SetStateAction<BudgetInput>>>();
 
-    render(<BudgetItemForm budget={budget} setBudget={setBudget} />, {
+    const {result} = renderHook(() =>
+      useForm<BudgetInput>({defaultValues: budget})
+    );
+
+    render(<BudgetItemForm control={result.current.control} />, {
       wrapper: ComponentTestWrapper
     });
 
-    expect(screen.getByTestId("accordion-Item 1")).toBeVisible();
-    expect(screen.getByTestId("accordion-Item 2")).toBeVisible();
-    expect(screen.getByTestId("accordion-Item 3")).toBeVisible();
-    expect(MOCK_ACCORDION).toBeCalledTimes(3);
+    expect(ExpandableAccordion).toBeCalledTimes(3);
+    expect(ExpandableAccordion).toBeCalledWith(
+      expect.objectContaining({children: expect.any(Object)}),
+      {}
+    );
 
     // test BudgetItemFormFields
-    const BudgetItemFormFieldsComponent = MOCK_ACCORDION.mock.calls[1][0]
-      .children as JSX.Element;
+    const BudgetItemFormFieldsComponent = (
+      ExpandableAccordion as jest.MockedFunction<typeof ExpandableAccordion>
+    ).mock.calls[1][0].children as JSX.Element;
     render(BudgetItemFormFieldsComponent, {
       wrapper: ComponentTestWrapper
     });
 
-    expect(screen.getByTestId("budgetItemFormFields-item-2")).toBeVisible();
-    expect(MOCK_BUDGET_ITEM_FORM_FIELDS).toBeCalledTimes(1);
-    expect(MOCK_BUDGET_ITEM_FORM_FIELDS).toBeCalledWith({
-      budgetItem: item2,
-      disabledCategories: [
-        TransactionCategory.EATING_OUT,
-        TransactionCategory.BILLS
-      ],
-      setBudgetItem: expect.any(Function)
-    });
-
-    // test setBudgetItem updates the right item
-    // in this case the second item
-    const setBudgetItem = BudgetItemFormFieldsComponent.props.setBudgetItem;
-    const newBudgetItem: BudgetItemInput = {
-      ...item2,
-      categories: [TransactionCategory.ENTERTAINMENT]
-    };
-    setBudgetItem(newBudgetItem);
-    expect(setBudget).toBeCalledTimes(1);
-
-    // test state change to set new budget from previous form values
-    const newBudgetFn = setBudget.mock.calls[0][0] as (
-      prevValues: BudgetInput
-    ) => BudgetInput;
-    const newBudget = newBudgetFn(budget);
-    expect(newBudget).toEqual({
-      ...EMPTY_BUDGET,
-      items: [ITEM_1, newBudgetItem, item3]
-    });
+    expect(BudgetItemFormFields).toBeCalledTimes(1);
+    expect(BudgetItemFormFields).toBeCalledWith(
+      {
+        disabledCategories: [
+          TransactionCategory.EATING_OUT,
+          TransactionCategory.BILLS
+        ],
+        control: result.current.control,
+        index: 1
+      },
+      {}
+    );
   });
 
   test("correctly adds an item to the form", async () => {
-    const setBudget =
-      jest.fn<React.Dispatch<React.SetStateAction<BudgetInput>>>();
+    const {result} = renderHook(() =>
+      useForm<BudgetInput>({defaultValues: BUDGET_WITH_ONE_EMPTY_ITEM})
+    );
 
-    render(<BudgetItemForm budget={EMPTY_BUDGET} setBudget={setBudget} />, {
+    render(<BudgetItemForm control={result.current.control} />, {
       wrapper: ComponentTestWrapper
     });
 
+    expect(result.current.getValues("items")).toHaveLength(1);
     fireEvent.press(screen.getByText("Add item"));
-    expect(setBudget).toBeCalledTimes(1);
-    const newBudgetFn = setBudget.mock.calls[0][0] as (
-      prevValues: BudgetInput
-    ) => BudgetInput;
-    const newBudget = newBudgetFn(EMPTY_BUDGET);
-    expect(newBudget).toEqual({
-      ...EMPTY_BUDGET,
-      items: [
-        ...EMPTY_BUDGET.items,
-        {
-          id: "unique-id",
-          name: "",
-          cap: "",
-          categories: []
-        }
-      ]
-    });
+    expect(result.current.getValues("items")).toHaveLength(2);
+    expect(result.current.getValues("items.1.id")).toEqual("unique-id");
+    expect(result.current.getValues("items.1.name")).toEqual("");
+    expect(result.current.getValues("items.1.cap")).toEqual("");
+    expect(result.current.getValues("items.1.categories")).toEqual([]);
   });
 });
