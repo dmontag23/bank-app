7fa2880dba6097c5e9d7f97c4d5131191c63424c
Dillon Montag <dillon@thoughtmachine.net>
2023-08-18 13:55:45 +0100

    Add toast context

---
+++ store/toast-context.spec.tsx
@@ -0,0 +1,77 @@
+import React, {useContext} from "react";
+import {describe, expect, test} from "@jest/globals";
+import {renderHook, waitFor} from "@testing-library/react-native";
+
+import ToastContext, {
+  Toast,
+  ToastContextProvider,
+  ToastType
+} from "./toast-context";
+
+import {TanstackQueryTestWrapper} from "../tests/mocks/utils";
+
+describe("ToastContext", () => {
+  const toast: Toast = {
+    id: "dummy-id",
+    message: "Toast message",
+    type: ToastType.LOG
+  };
+  test("defaults values correctly", () => {
+    const {result} = renderHook(() => useContext(ToastContext), {
+      wrapper: TanstackQueryTestWrapper
+    });
+
+    expect(result.current.toasts).toEqual([]);
+    expect(result.current.addToast).toEqual(expect.any(Function));
+    expect(result.current.clearToast).toEqual(expect.any(Function));
+    expect(result.current.clearAllToasts).toEqual(expect.any(Function));
+    expect(result.current.addToast(toast)).toBeUndefined();
+    expect(result.current.clearToast(toast.id)).toBeUndefined();
+    expect(result.current.clearAllToasts()).toBeUndefined();
+  });
+
+  test("ToastContextProvider adds and clears a toast", async () => {
+    const {result} = renderHook(() => useContext(ToastContext), {
+      wrapper: children => (
+        <ToastContextProvider>
+          {TanstackQueryTestWrapper(children)}
+        </ToastContextProvider>
+      )
+    });
+
+    expect(result.current.toasts).toEqual([]);
+
+    // add a toast
+    result.current.addToast(toast);
+    await waitFor(() => expect(result.current.toasts).toEqual([toast]));
+
+    // clear a toast
+    result.current.clearToast(toast.id);
+    await waitFor(() => expect(result.current.toasts).toEqual([]));
+  });
+
+  test("ToastContextProvider clears all toasts", async () => {
+    const {result} = renderHook(() => useContext(ToastContext), {
+      wrapper: children => (
+        <ToastContextProvider>
+          {TanstackQueryTestWrapper(children)}
+        </ToastContextProvider>
+      )
+    });
+
+    expect(result.current.toasts).toEqual([]);
+
+    // add 3 toasts
+    const toasts = [
+      toast,
+      {...toast, id: "dummy-id-2"},
+      {...toast, id: "dummy-id-3"}
+    ];
+    toasts.map(toastToAdd => result.current.addToast(toastToAdd));
+    await waitFor(() => expect(result.current.toasts).toEqual(toasts));
+
+    // clear all toasts
+    result.current.clearAllToasts();
+    await waitFor(() => expect(result.current.toasts).toEqual([]));
+  });
+});
