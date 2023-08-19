1a2dad2729df51903fd3ab3964fd32cb4bb4b356
Dillon Montag <dillon@thoughtmachine.net>
2023-08-19 16:02:35 +0100

    Make the expandable accordion more flexible

--- components/ui/ExpandableAccordion.spec.tsx
+++ components/ui/ExpandableAccordion.spec.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text} from "react-native-paper";
+import {MD3LightTheme, Text} from "react-native-paper";
 import {describe, expect, test} from "@jest/globals";
 import {fireEvent, render, screen} from "@testing-library/react-native";
 
@@ -7,8 +7,11 @@ import ExpandableAccordion from "./ExpandableAccordion";
 
 import {ComponentTestWrapper} from "../../tests/mocks/utils";
 
+// TODO: See if there's a good way to test the icon here
+// Also see if its possible to test the colors (other than the text colors already tested)
+
 describe("ExpandableAccordion component", () => {
-  test("renders an expanded accordion", async () => {
+  test("renders a closed accordion by default", async () => {
     render(
       <ExpandableAccordion title="Test title">
         <Text>Expanded</Text>
@@ -18,13 +21,52 @@ describe("ExpandableAccordion component", () => {
       }
     );
 
-    expect(screen.getByText("Test title")).toBeVisible();
+    const titleText = screen.getByText("Test title");
+    expect(titleText).toBeVisible();
+    expect(titleText).toHaveStyle({color: MD3LightTheme.colors.onSurface});
+
+    expect(screen.queryByText("Expanded")).toBeNull();
+  });
+
+  test("renders an expanded accordion", async () => {
+    render(
+      <ExpandableAccordion title="Test title" isInitiallyExpanded>
+        <Text>Expanded</Text>
+      </ExpandableAccordion>,
+      {
+        wrapper: ComponentTestWrapper
+      }
+    );
+
+    const titleText = screen.getByText("Test title");
+    expect(titleText).toBeVisible();
+    expect(titleText).toHaveStyle({color: MD3LightTheme.colors.primary});
+
+    expect(screen.getByText("Expanded")).toBeVisible();
+  });
+
+  test("accordion opens when clicked", async () => {
+    render(
+      <ExpandableAccordion title="Test title">
+        <Text>Expanded</Text>
+      </ExpandableAccordion>,
+      {
+        wrapper: ComponentTestWrapper
+      }
+    );
+
+    const accordionTitle = screen.getByText("Test title");
+    expect(accordionTitle).toBeVisible();
+    expect(screen.queryByText("Expanded")).toBeNull();
+
+    fireEvent.press(accordionTitle);
+
     expect(screen.getByText("Expanded")).toBeVisible();
   });
 
   test("accordion closes when clicked", async () => {
     render(
-      <ExpandableAccordion title="Test title">
+      <ExpandableAccordion title="Test title" isInitiallyExpanded>
         <Text>Expanded</Text>
       </ExpandableAccordion>,
       {
@@ -32,10 +74,34 @@ describe("ExpandableAccordion component", () => {
       }
     );
 
-    expect(screen.getByText("Test title")).toBeVisible();
+    const accordionTitle = screen.getByText("Test title");
+    expect(accordionTitle).toBeVisible();
     const accordionChild = screen.getByText("Expanded");
     expect(accordionChild).toBeOnTheScreen();
-    fireEvent.press(screen.getByText("Test title"));
+
+    fireEvent.press(accordionTitle);
+
     expect(accordionChild).not.toBeOnTheScreen();
+  });
+
+  test("accordion uses different color scheme", async () => {
+    render(
+      <ExpandableAccordion
+        title="Test title"
+        unselectedColor="red"
+        selectedColor="green">
+        <Text>Expanded</Text>
+      </ExpandableAccordion>,
+      {
+        wrapper: ComponentTestWrapper
+      }
+    );
+
+    const accordionTitle = screen.getByText("Test title");
+    expect(accordionTitle).toHaveStyle({color: "red"});
+
+    fireEvent.press(accordionTitle);
+
+    expect(accordionTitle).toHaveStyle({color: "green"});
   });
 });
--- components/ui/ExpandableAccordion.tsx
+++ components/ui/ExpandableAccordion.tsx
@@ -1,27 +1,59 @@
 import React, {ReactNode, useState} from "react";
 import {LayoutAnimation, StyleSheet} from "react-native";
 import {List, useTheme} from "react-native-paper";
+import {IconSource} from "react-native-paper/lib/typescript/src/components/Icon";
+
+const createIcon = (props: {color: string; icon: IconSource}) => (
+  <List.Icon {...props} />
+);
 
 type ExpandableAccordionProps = {
   title: string;
   children: ReactNode;
+  isInitiallyExpanded?: boolean;
+  icon?: IconSource;
+  unselectedColor?: string;
+  selectedColor?: string;
 };
 
-const ExpandableAccordion = ({title, children}: ExpandableAccordionProps) => {
+const ExpandableAccordion = ({
+  title,
+  children,
+  isInitiallyExpanded = false,
+  icon = undefined,
+  unselectedColor = undefined,
+  selectedColor = undefined
+}: ExpandableAccordionProps) => {
   const theme = useTheme();
 
-  const [isAccordionExpanded, setIsAccordionExpanded] = useState(true);
+  const [isAccordionExpanded, setIsAccordionExpanded] =
+    useState(isInitiallyExpanded);
+
+  const onSurfaceColor = unselectedColor ?? theme.colors.onSurface;
+  const primaryColor = selectedColor ?? theme.colors.primary;
 
   return (
     <List.Accordion
       title={title}
+      left={props => icon && createIcon({...props, icon})}
       onPress={() => {
         LayoutAnimation.easeInEaseOut();
         setIsAccordionExpanded(isExpanded => !isExpanded);
       }}
       expanded={isAccordionExpanded}
-      style={styles.accordionHeader}
-      theme={{colors: {background: theme.colors.elevation.level0}}}>
+      style={[styles.accordionHeader]}
+      theme={{
+        colors: {
+          // blend the background color with the container background color
+          background: theme.colors.elevation.level0,
+          // selected text and icon color
+          primary: primaryColor,
+          // text color
+          onSurface: onSurfaceColor,
+          // icon and arrow color
+          onSurfaceVariant: onSurfaceColor
+        }
+      }}>
       {children}
     </List.Accordion>
   );
