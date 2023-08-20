042d676c9867c50e387fa759e6f570af8e1f348b
Dillon Montag <dillon@thoughtmachine.net>
2023-08-20 11:08:37 +0100

    Add error badge to settings button

--- components/LoggedInScreens.spec.tsx
+++ components/LoggedInScreens.spec.tsx
@@ -4,13 +4,17 @@ import {fireEvent, render, screen} from "@testing-library/react-native";
 
 import BudgetsScreen from "./Budgets/BudgetsScreen";
 import LoggedInScreens from "./LoggedInScreens";
+import SettingsScreen from "./Settings/SettingsScreen";
 import TransactionsScreen from "./Transactions/TransactionsScreen";
 
+import ErrorContext from "../store/error-context";
 import {ComponentTestWrapper} from "../tests/mocks/utils";
 
 jest.mock("./Budgets/BudgetsScreen");
+jest.mock("./Settings/SettingsScreen");
 jest.mock("./Transactions/TransactionsScreen");
 
+// TODO: Come back and see if there's a way to test the icons
 describe("LoggedInScreens component", () => {
   test("renders the budgets scene as the default screen", () => {
     render(<LoggedInScreens />, {
@@ -24,7 +28,7 @@ describe("LoggedInScreens component", () => {
     expect(BudgetsScreen).toBeCalledTimes(1);
   });
 
-  test("renders the transactions scene", () => {
+  test("renders the transactions screen", () => {
     render(<LoggedInScreens />, {
       wrapper: ComponentTestWrapper
     });
@@ -40,19 +44,45 @@ describe("LoggedInScreens component", () => {
     expect(TransactionsScreen).toBeCalledTimes(1);
   });
 
-  test("renders the settings scene", () => {
+  test("renders the settings screen", () => {
     render(<LoggedInScreens />, {
       wrapper: ComponentTestWrapper
     });
 
-    const transactionButton = screen.getByRole("button", {
+    const settingsButton = screen.getByRole("button", {
       name: "Settings"
     });
-    expect(transactionButton).toBeVisible();
+    expect(settingsButton).toBeVisible();
 
     // TODO: Investigate why fireEvent.press does not work here
-    fireEvent(transactionButton, "click");
+    fireEvent(settingsButton, "click");
 
-    expect(screen.getByText("All settings")).toBeVisible();
+    expect(SettingsScreen).toBeCalledTimes(1);
+  });
+
+  test("checks the badge appears on the settings menu option if there are errors", () => {
+    render(
+      <ErrorContext.Provider
+        value={{
+          errors: [
+            {id: "1", error: ""},
+            {id: "2", error: ""}
+          ],
+          addError: () => {},
+          removeError: () => {},
+          errorModal: {
+            isVisible: false,
+            showModal: () => {},
+            hideModal: () => {}
+          }
+        }}>
+        <LoggedInScreens />
+      </ErrorContext.Provider>,
+      {
+        wrapper: ComponentTestWrapper
+      }
+    );
+
+    expect(screen.getByText("2")).toBeVisible();
   });
 });
