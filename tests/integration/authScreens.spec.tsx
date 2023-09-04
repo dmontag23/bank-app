2b20fd025c9ed2818ced3d3c9771b8b7e529c151
Dillon Montag <dillon@thoughtmachine.net>
2023-09-04 17:25:42 +0100

    Add integration tests for reconnect functionality

--- tests/integration/authScreens.spec.tsx
+++ tests/integration/authScreens.spec.tsx
@@ -1,52 +1,26 @@
 import React from "react";
+import {Linking} from "react-native";
 import WebView from "react-native-webview";
 import {fireEvent, render, screen, waitFor} from "testing-library/extension";
 import {describe, expect, jest, test} from "@jest/globals";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
 
 import {trueLayerAuthApi} from "../../api/axiosConfig";
-import ThirdPartyConnections from "../../components/AuthScreens/ThirdPartyConnections";
-import TruelayerAuthValidation from "../../components/AuthScreens/Truelayer/TruelayerAuthValidation";
-import TruelayerWebAuth from "../../components/AuthScreens/Truelayer/TruelayerWebAuth";
+import App from "../../App";
 import RootScreens from "../../components/RootScreens";
 import config from "../../config.json";
-import {RootStackParamList} from "../../types/screens";
 import {
-  AuthRedirectResponse,
   ConnectTokenPostRequest,
   ConnectTokenPostResponse
 } from "../../types/trueLayer/authAPI/auth";
 
 jest.mock("../../api/axiosConfig");
 
-describe("Root screen views - auth flow", () => {
-  // creating a mock navigator here because you can only access the
-  // Truelayer auth validation screen from a callback
-  const MockStackNavigator = createStackNavigator<RootStackParamList>();
-  const renderMockAuthScreens = (
-    params: Partial<AuthRedirectResponse> | undefined
-  ) => (
-    <NavigationContainer>
-      <MockStackNavigator.Navigator>
-        <MockStackNavigator.Screen
-          name="TruelayerAuthValidation"
-          component={TruelayerAuthValidation}
-          initialParams={params}
-        />
-        <MockStackNavigator.Screen
-          name="AppViews"
-          component={ThirdPartyConnections}
-        />
-        <MockStackNavigator.Screen
-          name="TruelayerWebAuth"
-          component={TruelayerWebAuth}
-        />
-      </MockStackNavigator.Navigator>
-    </NavigationContainer>
-  );
+// mock the deep linking mechanism in order to be able to test it
+jest.mock("react-native/Libraries/Linking/Linking");
 
+describe("Root screen views - auth flow", () => {
   test("renders the third party connections screen as a default when not logged in", async () => {
     render(
       <NavigationContainer>
@@ -77,7 +51,15 @@ describe("Root screen views - auth flow", () => {
   });
 
   test("renders Truelayer error page and allows users to try logging in again", async () => {
-    render(renderMockAuthScreens({error: "access_denied"}));
+    const deepLinkUrl = "bankapp://truelayer-callback?error=access_denied";
+
+    // Mock Linking.getInitialURL to return the deep link URL
+    // this will ensure the app starts as if it was opened with the specified url
+    (
+      Linking.getInitialURL as jest.MockedFunction<typeof Linking.getInitialURL>
+    ).mockResolvedValue(deepLinkUrl);
+
+    render(<App />);
 
     await waitFor(() =>
       expect(screen.getByText("An error has occurred")).toBeVisible()
@@ -106,10 +88,20 @@ describe("Root screen views - auth flow", () => {
     );
   });
 
-  test("renders unknown error page and allows users to return to the home screen", () => {
-    render(renderMockAuthScreens(undefined));
+  test("renders unknown error page and allows users to return to the home screen", async () => {
+    const deepLinkUrl = "bankapp://truelayer-callback";
 
-    expect(screen.getByText("An error has occurred")).toBeVisible();
+    // Mock Linking.getInitialURL to return the deep link URL
+    // this will ensure the app starts as if it was opened with the specified url
+    (
+      Linking.getInitialURL as jest.MockedFunction<typeof Linking.getInitialURL>
+    ).mockResolvedValue(deepLinkUrl);
+
+    render(<App />);
+
+    await waitFor(() =>
+      expect(screen.getByText("An error has occurred")).toBeVisible()
+    );
     expect(screen.getByText("The error is unknown")).toBeVisible();
     expect(screen.getByText("Try again")).toBeVisible();
 
@@ -124,8 +116,6 @@ describe("Root screen views - auth flow", () => {
     expect(screen.getByText("Connect to Truelayer")).toBeVisible();
   });
 
-  // TODO: Maybe find a way to use deep linking here instead of creating a mock
-  // stack navigator
   test("displays error page on failed connect to Truelayer endpoint", async () => {
     (
       trueLayerAuthApi.post as jest.MockedFunction<
@@ -138,9 +128,16 @@ describe("Root screen views - auth flow", () => {
       Promise.reject("Cannot connect to the truelayer api")
     );
 
-    render(
-      renderMockAuthScreens({code: "truelayer-dummy-code", scope: "accounts"})
-    );
+    const deepLinkUrl =
+      "bankapp://truelayer-callback?code=truelayer-dummy-code&scope=accounts";
+
+    // Mock Linking.getInitialURL to return the deep link URL
+    // this will ensure the app starts as if it was opened with the specified url
+    (
+      Linking.getInitialURL as jest.MockedFunction<typeof Linking.getInitialURL>
+    ).mockResolvedValue(deepLinkUrl);
+
+    render(<App />);
 
     await waitFor(() =>
       expect(screen.getByText("An error has occurred")).toBeVisible()
@@ -148,8 +145,6 @@ describe("Root screen views - auth flow", () => {
     expect(screen.getByText("The error is unknown")).toBeVisible();
   });
 
-  // TODO: Maybe find a way to use deep linking here instead of creating a mock
-  // stack navigator
   test("stores valid Truelayer tokens and navigates to the home budgets page", async () => {
     const mockAccessToken = "valid-truelayer-access-token";
     const mockRefreshToken = "valid-truelayer-refresh-token";
@@ -168,22 +163,146 @@ describe("Root screen views - auth flow", () => {
       scope: "info"
     }));
 
-    render(
-      renderMockAuthScreens({code: "truelayer-dummy-code", scope: "accounts"})
-    );
+    const deepLinkUrl =
+      "bankapp://truelayer-callback?code=truelayer-dummy-code&scope=accounts";
 
-    // this goes back to the Third Party Auth screen because of how the mock auth
-    // screen function is setup. Normally it should go the the main budgets screen
+    // Mock Linking.getInitialURL to return the deep link URL
+    // this will ensure the app starts as if it was opened with the specified url
+    (
+      Linking.getInitialURL as jest.MockedFunction<typeof Linking.getInitialURL>
+    ).mockResolvedValue(deepLinkUrl);
+
+    render(<App />);
+
     await waitFor(() =>
-      expect(
-        screen.getByText("Please connect to the following services")
-      ).toBeVisible()
+      expect(screen.getByText("Please select a budget")).toBeVisible()
     );
     expect(await AsyncStorage.getItem("truelayer-auth-token")).toBe(
       mockAccessToken
     );
     expect(await AsyncStorage.getItem("truelayer-refresh-token")).toBe(
       mockRefreshToken
+    );
+  });
+
+  test("allows users to go back to the app on an unknown error page if a token already exists", async () => {
+    const mockAccessToken = "dummy-truelayer-auth-token";
+    const mockRefreshToken = "dummy-truelayer-refresh-token";
+    await AsyncStorage.multiSet([
+      ["truelayer-auth-token", mockAccessToken],
+      ["truelayer-refresh-token", mockRefreshToken]
+    ]);
+
+    const deepLinkUrl = "bankapp://truelayer-callback";
+
+    // Mock Linking.getInitialURL to return the deep link URL
+    // this will ensure the app starts as if it was opened with the specified url
+    (
+      Linking.getInitialURL as jest.MockedFunction<typeof Linking.getInitialURL>
+    ).mockResolvedValue(deepLinkUrl);
+
+    render(<App />);
+
+    await waitFor(() =>
+      expect(screen.getByText("An error has occurred")).toBeVisible()
+    );
+
+    const returnToHomeScreenButton = screen.getByText("Return to home screen");
+    expect(returnToHomeScreenButton).toBeVisible();
+
+    fireEvent.press(returnToHomeScreenButton);
+
+    expect(screen.getByText("Please select a budget")).toBeVisible();
+    expect(await AsyncStorage.getItem("truelayer-auth-token")).toBe(
+      mockAccessToken
+    );
+    expect(await AsyncStorage.getItem("truelayer-refresh-token")).toBe(
+      mockRefreshToken
+    );
+  });
+
+  test("allows users to try again an unknown error page if a token already exists", async () => {
+    const mockAccessToken = "dummy-truelayer-auth-token";
+    const mockRefreshToken = "dummy-truelayer-refresh-token";
+    await AsyncStorage.multiSet([
+      ["truelayer-auth-token", mockAccessToken],
+      ["truelayer-refresh-token", mockRefreshToken]
+    ]);
+
+    const deepLinkUrl = "bankapp://truelayer-callback";
+
+    // Mock Linking.getInitialURL to return the deep link URL
+    // this will ensure the app starts as if it was opened with the specified url
+    (
+      Linking.getInitialURL as jest.MockedFunction<typeof Linking.getInitialURL>
+    ).mockResolvedValue(deepLinkUrl);
+
+    render(<App />);
+
+    await waitFor(() =>
+      expect(screen.getByText("An error has occurred")).toBeVisible()
+    );
+
+    const tryAgainButton = screen.getByText("Try again");
+    expect(tryAgainButton).toBeVisible();
+
+    fireEvent.press(tryAgainButton);
+
+    expect(WebView).toBeCalledTimes(1);
+    expect(WebView).toBeCalledWith(
+      {
+        hideKeyboardAccessoryView: true,
+        source: {
+          uri: config.integrations.trueLayer.authLink
+        }
+      },
+      {}
+    );
+  });
+
+  test("stores new tokens on reauthorization", async () => {
+    const mockAccessToken = "dummy-truelayer-auth-token";
+    const mockRefreshToken = "dummy-truelayer-refresh-token";
+    await AsyncStorage.multiSet([
+      ["truelayer-auth-token", mockAccessToken],
+      ["truelayer-refresh-token", mockRefreshToken]
+    ]);
+
+    (
+      trueLayerAuthApi.post as jest.MockedFunction<
+        typeof trueLayerAuthApi.post<
+          ConnectTokenPostRequest,
+          ConnectTokenPostResponse
+        >
+      >
+    ).mockImplementation(async () => ({
+      access_token: "new-access-token",
+      expires_in: 3600,
+      refresh_token: "new-refresh-token",
+      token_type: "Bearer",
+      scope: "info"
+    }));
+
+    const deepLinkUrl =
+      "bankapp://truelayer-callback?code=dummy-truelayer-code&scope=accounts";
+
+    // Mock Linking.getInitialURL to return the deep link URL
+    // this will ensure the app starts as if it was opened with the specified url
+    (
+      Linking.getInitialURL as jest.MockedFunction<typeof Linking.getInitialURL>
+    ).mockResolvedValue(deepLinkUrl);
+
+    render(<App />);
+
+    await waitFor(() =>
+      expect(screen.getByText("Please select a budget")).toBeVisible()
+    );
+
+    expect(await AsyncStorage.getItem("truelayer-auth-token")).toBe(
+      "new-access-token"
+    );
+    expect(await AsyncStorage.getItem("truelayer-refresh-token")).toBe(
+      "new-refresh-token"
     );
   });
 });
--- tests/integration/components/Settings.spec.tsx
+++ tests/integration/components/Settings.spec.tsx
@@ -1,22 +1,79 @@
-import React from "react";
-import {fireEvent, render, screen, waitFor} from "testing-library/extension";
+import React, {ReactNode} from "react";
+import WebView from "react-native-webview";
+import {
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+  waitFor
+} from "testing-library/extension";
 import {describe, expect, test} from "@jest/globals";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {MaterialBottomTabScreenProps} from "@react-navigation/material-bottom-tabs";
+import {
+  CompositeScreenProps,
+  NavigationContainer,
+  useNavigation
+} from "@react-navigation/native";
+import {StackScreenProps} from "@react-navigation/stack";
 
 import ErrorModal from "../../../components/errors/ErrorModal";
+import RootScreens from "../../../components/RootScreens";
 import SettingsScreen from "../../../components/Settings/SettingsScreen";
+import config from "../../../config.json";
+import {LoggedInTabParamList, RootStackParamList} from "../../../types/screens";
 
 describe("Settings Screen", () => {
   test("renders all items on the homepage", () => {
-    render(<SettingsScreen />);
+    const customWrapper = (children: ReactNode) => (
+      <NavigationContainer>{children}</NavigationContainer>
+    );
+
+    const {result} = renderHook(
+      () =>
+        useNavigation<
+          CompositeScreenProps<
+            MaterialBottomTabScreenProps<LoggedInTabParamList, "Settings">,
+            StackScreenProps<RootStackParamList>
+          >
+        >(),
+      {customWrapper}
+    );
+
+    render(
+      <SettingsScreen
+        route={{key: "", name: "Settings"}}
+        navigation={result.current.navigation}
+      />
+    );
 
     expect(screen.getByText("Settings")).toBeVisible();
+    expect(screen.getByText("Reconnect to Truelayer")).toBeVisible();
     expect(screen.getByText("Show Errors")).toBeVisible();
   });
 
   test("can show all app errors", async () => {
+    const customWrapper = (children: ReactNode) => (
+      <NavigationContainer>{children}</NavigationContainer>
+    );
+
+    const {result} = renderHook(
+      () =>
+        useNavigation<
+          CompositeScreenProps<
+            MaterialBottomTabScreenProps<LoggedInTabParamList, "Settings">,
+            StackScreenProps<RootStackParamList>
+          >
+        >(),
+      {customWrapper}
+    );
+
     render(
       <>
-        <SettingsScreen />
+        <SettingsScreen
+          route={{key: "", name: "Settings"}}
+          navigation={result.current.navigation}
+        />
         <ErrorModal />
       </>
     );
@@ -27,6 +84,46 @@ describe("Settings Screen", () => {
     fireEvent.press(showErrorsButton);
 
     await waitFor(() => expect(screen.getByText("Errors")).toBeVisible());
+  });
+
+  test("can reconnect to Truelayer", async () => {
+    await AsyncStorage.setItem("truelayer-auth-token", "dummy-truelayer-token");
+
+    render(
+      <NavigationContainer>
+        <RootScreens />
+      </NavigationContainer>
+    );
+
+    // navigate to the settings screen
+    await waitFor(() =>
+      expect(
+        screen.getByRole("button", {
+          name: "Settings"
+        })
+      ).toBeVisible()
+    );
+    const settingsButton = screen.getByRole("button", {
+      name: "Settings"
+    });
+    // TODO: investigate why fireEvent.press does not work here
+    fireEvent(settingsButton, "click");
+
+    const getTruelayerButton = screen.getByText("Reconnect to Truelayer");
+    expect(getTruelayerButton).toBeVisible();
+
+    fireEvent.press(getTruelayerButton);
+
+    expect(WebView).toBeCalledTimes(1);
+    expect(WebView).toBeCalledWith(
+      {
+        hideKeyboardAccessoryView: true,
+        source: {
+          uri: config.integrations.trueLayer.authLink
+        }
+      },
+      {}
+    );
   });
 
   // TODO: Come back and add a test for the error badge
