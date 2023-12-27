01b7e4759e6bbff4ce549773c693e94610396714
Dillon Montag <dillon@thoughtmachine.net>
2023-12-27 08:39:57 -0700

    Upgrade rn paper to version 5.11.5

--- components/AuthScreens/Truelayer/TruelayerAuthValidation.tsx
+++ components/AuthScreens/Truelayer/TruelayerAuthValidation.tsx
@@ -77,9 +77,7 @@ const TruelayerAuthValidation = ({
         onPress={() => navigation.replace("TruelayerWebAuth")}>
         Try again
       </Button>
-      {/* TODO: This button should have mode="elevated" but there is currently an issue with react native paper.
-      See https://github.com/callstack/react-native-paper/issues/4224  */}
-      <Button onPress={() => navigation.replace("AppViews")}>
+      <Button mode="elevated" onPress={() => navigation.replace("AppViews")}>
         Return to home screen
       </Button>
     </SafeAreaView>
